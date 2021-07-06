92dcf13b25363487ece04b66215e742aaf01654d
RaviGprec <ravi947kumar@gmail.com>
2021-07-06 16:54:15 +0530

    Update OptionalDemo.java

--- Optional/OptionalDemo.java
+++ Optional/OptionalDemo.java
@@ -1,6 +1,11 @@
 import java.util.*;
 import java.util.stream.Collectors;
 public class OptionalDemo {
+	public static Optional<Customer> getCustomerByEmail(String email) throws Exception {
+		List<Customer> customers = CustomerDataBase.getAllCustomers();
+		//return customers.stream().filter(c -> c.getEmailId() ==email).findAny().orElseThrow(()->new Exception("no customer present with this email id"));
+		return customers.stream().filter(c -> c.getEmailId() ==email).findAny();
+	}
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		Customer c=new Customer(1,"Ravi","HYD",null);
@@ -9,6 +14,7 @@ public class OptionalDemo {
 		//Below code to handle NullPointExceptin
 		Optional<String> OptionalemailId = Optional.ofNullable(c.getEmailId());
 		System.out.println(OptionalemailId.map(String::toUpperCase).orElseGet(()->"Default eamil"));
+		System.out.println(getCustomerByEmail("pk"));
 	}
 
 } 
